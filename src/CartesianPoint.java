055e20a06d04ae4ac6e026a3de141ee453a25d4d
amir.afghani <amir.afghani@workday.com>
2018-07-31 11:08:15 -0700

    Cleanup

--- src/CartesianPoint.java
+++ src/CartesianPoint.java
@@ -1,13 +1,42 @@
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 public class CartesianPoint {
 
-    private int x;
-    private int y;
+    private final int x;
+    private final int y;
 
-    CartesianPoint(final int x,
-                   final int y) {
+    private static final Map<String, CartesianPoint> POINTS_CACHE = initPointsCache();
+
+    private CartesianPoint(final int x,
+                           final int y) {
         this.x = x;
         this.y = y;
     }
+
+    static CartesianPoint getPoint(final int x,
+                                   final int y) {
+        final String key = x + "_" + y;
+        if (!POINTS_CACHE.containsKey(key)) {
+            throw new RuntimeException("Specified point doesn't exist in map");
+        } else {
+            return POINTS_CACHE.get(key);
+        }
+    }
+
+    private static Map<String, CartesianPoint> initPointsCache() {
+        final Map<String, CartesianPoint> pointsCache = new HashMap<>();
+        for(int i = 0; i < World.WIDTH; i++) {
+            for(int j = 0; j < World.HEIGHT; j++) {
+                pointsCache.put(i + "_" +j, new CartesianPoint(i, j));
+            }
+        }
+        System.out.println("Points cache filled up, size = " +pointsCache.size());
+        return Collections.unmodifiableMap(pointsCache);
+    }
+
 
     int getX() {
         return this.x;
@@ -17,11 +46,17 @@ public class CartesianPoint {
         return this.y;
     }
 
-    void setX(final int x) {
-        this.x = x;
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final CartesianPoint point = (CartesianPoint) o;
+        return this.x == point.x &&
+                this.y == point.y;
     }
 
-    void setY(final int y) {
-        this.y = y;
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.x, this.y);
     }
 }
--- src/RandomWalker.java
+++ src/RandomWalker.java
@@ -3,13 +3,14 @@ import java.util.Random;
 class RandomWalker {
 
     private final int size;
-    private final CartesianPoint currentLocation;
     private final World gameWorld;
 
-    private static final Random R = new Random(1000);
+    private CartesianPoint currentLocation;
+
+    private static final Random R = new Random();
 
     RandomWalker(final World gameWorld) {
-        this.currentLocation = new CartesianPoint(World.WIDTH /2, World.HEIGHT /2);
+        this.currentLocation = CartesianPoint.getPoint(World.WIDTH / 2, World.HEIGHT / 2);
         this.size = 1;
         this.gameWorld = gameWorld;
     }
@@ -23,34 +24,33 @@ class RandomWalker {
     }
 
     void step() {
-        final CartesianPoint lastStep = this.currentLocation;
-        final CartesianPoint nextStep = new CartesianPoint(lastStep.getX(), lastStep.getY());
+        CartesianPoint nextStep = null;
         final int choice = R.nextInt(4);
-        switch (choice) {
-            case 0:
-                if(nextStep.getX() + 1 < this.gameWorld.getWidth()) {
-                    nextStep.setX(nextStep.getX() + 1);
-                }
-                break;
-            case 1:
-                if(nextStep.getX() - 1 > 0) {
-                    nextStep.setX(nextStep.getX() - 1);
-                }
-                break;
-            case 2:
-                if(nextStep.getY() + 1 < this.gameWorld.getHeight()) {
-                    nextStep.setY(nextStep.getY() + 1);
-                }
-                break;
-            case 3:
-                if(nextStep.getY() - 1 > 0) {
-                    nextStep.setY(nextStep.getY() - 1);
-                }
-                break;
-            default:
-                throw new RuntimeException("what!?");
+
+        if(choice == 0) {
+            if (this.currentLocation.getX() + 1 < this.gameWorld.getWidth()) {
+                nextStep = CartesianPoint.getPoint(this.getCurrentLocation().getX() + 1,
+                        this.getCurrentLocation().getY());
+            }
+        } else if(choice == 1) {
+            if (this.currentLocation.getX() - 1 > 0) {
+                nextStep = CartesianPoint.getPoint(this.getCurrentLocation().getX() - 1,
+                        this.getCurrentLocation().getY());
+            }
+        } else if(choice == 2) {
+            if (this.currentLocation.getY() + 1 < this.gameWorld.getHeight()) {
+                nextStep = CartesianPoint.getPoint(this.getCurrentLocation().getX(),
+                        this.getCurrentLocation().getY() + 1);
+            }
+        } else if(choice == 3){
+            if (this.currentLocation.getY() - 1 > 0) {
+                nextStep = CartesianPoint.getPoint(this.getCurrentLocation().getX(),
+                        this.getCurrentLocation().getY() - 1);
+            }
         }
-        this.currentLocation.setX(nextStep.getX());
-        this.currentLocation.setY(nextStep.getY());
+
+        if(nextStep != null) {
+            this.currentLocation = nextStep;
+        }
     }
 }
